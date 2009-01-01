wsilly/tomcat-connectors

commit d6a4c92be405cb4c654993c01545264230b70f6c

Sync methods implemented natively with their Java references.
Fixes BZ 46457 and several other inconsistencies:

Changes to Java code:

Buffer:       rename alloc() to malloc()
Buffer:       change return type of size() from int to long
File:         add getStat() and getInfo()
Registry:     change val parameter in setValueJ() from int to long
Socket:       add acceptx()
SSLContext:   add setRandom()
SSLContext:   rename setShutdowType() to setShutdownType()
Stdlib:       change num parameter in calloc() from long to int

Changes to native code:

address.c:    change return type of equal() from int to boolean
file.c:       change pool parameter in mktemp() from int to long
lock.c:       refer to Java method destroy instead of destoy in
              Global and Lock
multicast.c:  refer to Java class Multicast instead of Mulicast
ssl.c:        add randSet() in non-OpenSSL case
sslcontext.c: whitespace


git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@730523 13f79535-47bb-0310-9956-ffa450edef68

jni/java/org/apache/tomcat/jni/SSLContext.java
@@ -226,6 +226,13 @@ public final class SSLContext {
         throws Exception;
 
     /**
+     * Set file for randomness
+     * @param ctx Server or Client context to use.
+     * @param file random file.
+     */
+    public static native void setRandom(long ctx, String file)
+
+    /**
      * Set SSL connection shutdown type
      * <br />
      * The following levels are available for level:
@@ -237,7 +244,7 @@ public final class SSLContext {
      * @param ctx Server or Client context to use.
      * @param type Shutdown type to use.
      */
-    public static native void setShutdowType(long ctx, int type);
+    public static native void setShutdownType(long ctx, int type);
 
     /**
      * Set Type of Client Certificate verification and Maximum depth of CA Certificates

jni/java/org/apache/tomcat/jni/Buffer.java
@@ -32,7 +32,7 @@ public class Buffer {
      * @param size The amount of memory to allocate
      * @return The ByteBuffer with allocated memory
      */
-    public static native ByteBuffer alloc(int size);
+    public static native ByteBuffer malloc(int size);
 
     /**
      * Allocate a new ByteBuffer from memory and set all of the memory to 0
@@ -86,6 +86,6 @@ public class Buffer {
      * Returns the allocated memory size of the ByteBuffer.
      * @param buf Previously allocated ByteBuffer.
      */
-    public static native int size(ByteBuffer buf);
+    public static native long size(ByteBuffer buf);
 
 }

jni/java/org/apache/tomcat/jni/File.java
@@ -693,11 +693,30 @@ public class File {
     public static native int stat(FileInfo finfo, String fname, int wanted, long pool);
 
     /**
+     * Get the specified file's stats.  The file is specified by filename,
+     * instead of using a pre-opened file.
+     * @param fname The name of the file to stat.
+     * @param wanted The desired apr_finfo_t fields, as a bit flag of APR_FINFO_ values
+     * @param pool the pool to use to allocate the new file.
+     * @return FileInfo object.
+     */
+    public static native FileInfo getStat(String fname, int wanted, long pool);
+
+    /**
      * Get the specified file's stats.
      * @param finfo Where to store the information about the file.
      * @param wanted The desired apr_finfo_t fields, as a bit flag of APR_FINFO_ values
      * @param thefile The file to get information about.
      */
     public static native int infoGet(FileInfo finfo, int wanted, long thefile);
+
+
+    /**
+     * Get the specified file's stats.
+     * @param wanted The desired apr_finfo_t fields, as a bit flag of APR_FINFO_ values
+     * @param thefile The file to get information about.
+     * @return FileInfo object.
+     */
+    public static native FileInfo getInfo(int wanted, long thefile);
 
 }

jni/java/org/apache/tomcat/jni/Registry.java
@@ -156,7 +156,7 @@ public class Registry {
      * @param val The the value to set
      * @return If the function succeeds, the return value is 0
      */
-    public static native int setValueJ(long key, String name, int val);
+    public static native int setValueJ(long key, String name, long val);
 
     /**
      * Set the Registry value for REG_SZ

jni/java/org/apache/tomcat/jni/Socket.java
@@ -170,6 +170,16 @@ public class Socket {
      *          made the connection request.  This is the socket which should
      *          be used for all future communication.
      */
+    public static native long acceptx(long sock, long pool)
+        throws Exception;
+
+    /**
+     * Accept a new connection request
+     * @param sock The socket we are listening on.
+     * @return  A copy of the socket that is connected to the socket that
+     *          made the connection request.  This is the socket which should
+     *          be used for all future communication.
+     */
     public static native long accept(long sock)
         throws Exception;
 

jni/java/org/apache/tomcat/jni/Stdlib.java
@@ -67,7 +67,7 @@ public class Stdlib {
      * @param num Number of elements.
      * @param sz Length in bytes of each element.
      */
-    public static native long calloc(long num, int sz);
+    public static native long calloc(int num, int sz);
 
     /**
      * Deallocates or frees a memory block.

jni/native/src/address.c
@@ -103,8 +103,8 @@ cleanup:
     return P2J(sa);
 }
 
-TCN_IMPLEMENT_CALL(jint, Address, equal)(TCN_STDARGS,
-                                         jlong a, jlong b)
+TCN_IMPLEMENT_CALL(jboolean, Address, equal)(TCN_STDARGS,
+                                             jlong a, jlong b)
 {
     apr_sockaddr_t *sa = J2P(a, apr_sockaddr_t *);
     apr_sockaddr_t *sb = J2P(b, apr_sockaddr_t *);

jni/native/src/file.c
@@ -92,7 +92,7 @@ cleanup:
 
 TCN_IMPLEMENT_CALL(jlong, File, mktemp)(TCN_STDARGS, jstring templ,
                                       jint flags,
-                                      jint pool)
+                                      jlong pool)
 {
     apr_pool_t *p = J2P(pool, apr_pool_t *);
     apr_file_t *f = NULL;

commit 036ae889e39f752fe55dff9704d501421d9cc2e7

Fix syntax error introduced in r730523.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@730528 13f79535-47bb-0310-9956-ffa450edef68

jni/java/org/apache/tomcat/jni/SSLContext.java
@@ -230,7 +230,7 @@ public final class SSLContext {
      * @param ctx Server or Client context to use.
      * @param file random file.
      */
-    public static native void setRandom(long ctx, String file)
+    public static native void setRandom(long ctx, String file);
 
     /**
      * Set SSL connection shutdown type